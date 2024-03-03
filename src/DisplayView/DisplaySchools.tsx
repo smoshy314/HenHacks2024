d59951f6eda8e80cf0325c91c372026ae750bbfe
btbf569708@gmail.com <btbf569708@gmail.com>
2024-03-03 02:36:43 -0500

    finished college drop down

--- src/DisplayView/DisplaySchools.tsx
+++ src/DisplayView/DisplaySchools.tsx
@@ -1,2 +1,29 @@
+import React from "react";
 import accessAllSchools from "../data/AllSchools.json";
+import { School } from "../Interfaces/school";
+import { Dropdown } from "react-bootstrap";
 
+const DisplaySchools = () => {
+    const schoolList: School[] = accessAllSchools.map(
+        (aSchool): School => ({
+            ...aSchool,
+            name: aSchool.name,
+            country: aSchool.country
+        })
+    );
+
+    return (
+        <Dropdown>
+            <Dropdown.Toggle id="dropdown">Schools:</Dropdown.Toggle>
+            <Dropdown.Menu>
+                {
+                    schoolList.map((school, index) => (
+                        <Dropdown.Item key={index}>{school.name}</Dropdown.Item>
+                    ))
+                }
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+};
+
+export default DisplaySchools;
