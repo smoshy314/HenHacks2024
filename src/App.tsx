8321c2b14b846c575d02c983c95a92f3e50f24b8
Malika <113390791+malikaiyer@users.noreply.github.com>
2024-03-03 06:26:38 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,7 @@
-// import React from 'react';
-// // import logo from './logo.svg';
-// import Header from './Header';
-
-// import './App.css';
-// import { ChatView } from './components/ChatView';
-// import FilterComponent from './DisplayView/FilterComponent';
-// import Collapsible from 'react-collapsible'; //npm install react-collapsible
-// import DisplaySchools from './DisplayView/DisplaySchools';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Header>
-//       {/* <header className="App-header"> */}
-//       <section id ="about">
-//         <div className="App-title">AccessiTech</div>
-//         <Collapsible className="App-help" trigger=" Need Help❓" style={{ fontSize: '16px' }}>
-//           <p  style={{ fontSize: '16px' }}>
-//             AccessiTech is a web application aimed at providing up-to-date information on accessibility features to locations. 
-//             Such as: wheelchair accessible ramps, elevators and gender-inclusive bathrooms. 
-//             AccessiTech also aims to connect you with others through your school's student-run organizations and nearby groups focused around accessibility.
-//             Use our map to view locations of elevators or use our text feature to connect with people nearby!
-//           </p>
-//           <p  style={{ fontSize: '16px' }}>
-//             Available features:
-//             <ul>
-//               <li>
-//                 Choose your school from our dropdown to view the chat options and map/list views
-//               </li>
-//               <li>
-//                 View a 
-//               </li>
-//             </ul>
-//           </p>
-//       </Collapsible>
-//       </section>
-//       {/* </header> */}
-//         <DisplaySchools />
-//         <br></br>
-//         <section id="navigation">
-//           <FilterComponent />
-//         </section>
-//         <hr></hr>
-//         <section id="connect">
-//           <ChatView></ChatView>
-//         </section>
-//         <section id="contact">contact us!</section>
-//       </Header>
-//     </div>
-//   );
-// }
-  
-// export default App;
-
 import React from 'react';
 import { ChatView } from './components/ChatView';
 import FilterComponent from './DisplayView/FilterComponent';
-import { DisplayMap } from './DisplayView/DisplayMap';
+// import { DisplayMap } from './DisplayView/DisplayMap';
 import Collapsible from 'react-collapsible'; //npm install react-collapsible
 import DisplaySchools from './DisplayView/DisplaySchools';
 import styled from 'styled-components'; // Import styled-components here
