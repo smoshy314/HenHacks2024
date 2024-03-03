89b63afad943fdbd4f2e24e7f618bbd6af73fe2e
malikaiyer <malikakarthik@gmail.com>
2024-03-03 03:26:18 -0500

    css changes for list view

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <br></br>
-        <div className="App-title">AccessiTek</div>
+        <div className="App-title">AccessiTech</div>
         <br></br>
         <Collapsible className="App-help" trigger="❓">
-          <p>
+          <small>
             AccessiTek is a web application aimed at providing up-to-date information on accessibility features to locations. 
             Such as: wheelchair accessible ramps, elevators and gender-inclusive bathrooms. 
             AccessiTek also aims to connect you with others through your school's student-run organizations and nearby groups focused around accessibility.
             Use our map to view locations of elevators or use our text feature to 
-          </p>
+          </small>
         </Collapsible>
         <br></br>
       </header>
       <FilterComponent />
+      <hr></hr>
       <ChatView></ChatView>
     </div>
   );
