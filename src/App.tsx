71ee390294227bbafe8c65301745e7473cec2c8c
btbf569708@gmail.com <btbf569708@gmail.com>
2024-03-02 21:23:39 -0500

    testing to see if github cp works

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import { ChatView } from './components/ChatView';
 
@@ -13,5 +13,5 @@ function App() {
     </div>
   );
 }
-
+  
 export default App;
