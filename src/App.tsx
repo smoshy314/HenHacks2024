88d7588292dd80d6e736d426c904718388095b5c
westa <westa@udel.edu>
2024-03-03 02:27:48 -0500

    The issue was the github pull from npm install and added changes lost from before

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,21 @@ import React from 'react';
 import './App.css';
 import { ChatView } from './components/ChatView';
 import FilterComponent from './DisplayView/FilterComponent';
+import Collapsible from 'react-collapsible';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <br></br>
+        <div className="App-title">AccessiTek</div>
+        <br></br>
+        <Collapsible className="App-help" trigger="❓">
+          <p>
+            Here's how to use our site!
+          </p>
+        </Collapsible>
+        <br></br>
       </header>
       <FilterComponent />
       <ChatView></ChatView>
--- src/components/ChatBoxRight.tsx
+++ src/components/ChatBoxRight.tsx
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
-import { Group } from "./ChatView";
+import React, { useEffect, useState } from "react";
+import { Group, blankGroup } from "./ChatView";
 import { Chat } from "./ChatView";
 import { Form, Button } from "react-bootstrap";
 import { MessagesOutput } from "./ChatBoxMessage";
 
 export interface ChatBoxRightProps {
-  group: Group,
-  groups: Group[],
-  setGroups: (group: Group[]) => void;};
+    group: Group,
+    groups: Group[],
+    setCurrentGroup: (grp: Group) => void,
+    setGroups: (group: Group[]) => void
+}
 
-export function ChatBoxRight({group, groups, setGroups}:ChatBoxRightProps): JSX.Element{
-    const [currentMessage, setCurrentMessage] = useState<string>(""); 
-    
+export function ChatBoxRight({group, groups, setCurrentGroup, setGroups}:ChatBoxRightProps): JSX.Element{
+    const [currentMessage, setCurrentMessage] = useState<string>("");
+
     function handleFormEdit(e: React.ChangeEvent<HTMLInputElement>){
       setCurrentMessage(e.target.value);
     };
@@ -21,15 +23,16 @@ export function ChatBoxRight({group, groups, setGroups}:ChatBoxRightProps): JSX.
       const newChatlog: Chat[] = group.chatlog.concat(newChat);
       const newGroup: Group = {...group, chatlog: newChatlog};
       setGroups(groups.map((g)=>g.name === group.name ? newGroup : g));
+      setCurrentGroup(newGroup);
     }
+
     return (
-      <div className="ChatView">
+      <div className="ChatViewRight">
+        <MessagesOutput chatlog={group.chatlog}/>
         <Form.Group>
           <Form.Control placeholder="Message" value={currentMessage} onChange={handleFormEdit} />
         </Form.Group>
         <Button onClick={handleSendMessage}>➡️</Button>
-        <MessagesOutput chatlog={group.chatlog}/>
-
       </div>
     );
 }
